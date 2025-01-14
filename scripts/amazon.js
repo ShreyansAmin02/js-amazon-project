de60aeb721d6083e8edba0202cd48ee4bc6c0bc5
ShreyansAmin02 <shreyans.amin02@gmail.com>
2025-01-14 10:20:44 +1000

    Initial Commit

---
+++ scripts/amazon.js
@@ -0,0 +1,101 @@
+let productsHTML = '';
+let timerID;
+
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${product.image}">
+        </div>
+
+        <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
+          <div class="product-rating-count link-primary">
+          ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          $${(product.priceCents / 100).toFixed(2)}
+        </div>
+
+        <div class="product-quantity-container">
+          <select class="js-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart js-added-to-cart-${product.id}">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id="${product.id}">
+          Add to Cart
+        </button>
+      </div>
+  `;
+});
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const { productId } = button.dataset; // get the product by its id (button.dataset contians ID as such this shorthand method works)
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`); // select the quanity from 1-10
+    const addedSection = document.querySelector(`.js-added-to-cart-${productId}`);
+
+    addedSection.classList.add('added');
+
+    clearTimeout(timerID);
+    timerID = setTimeout(() => {
+      added.classList.remove('added');
+    }, 2000);
+
+    const quantity = Number(quantitySelector.value);
+
+    let matchingItem = '';
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    } else {
+      cart.push({ // add the product and quantity to the cart array
+        productId,
+        quantity
+      });
+    }
+
+    let cartQuanity = 0;
+
+    console.log(cart);
+
+    cart.forEach((item) => {
+      cartQuanity += item.quantity; //adds all the quanitites and saves to varaible 
+    });
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuanity;
+  });
+});
