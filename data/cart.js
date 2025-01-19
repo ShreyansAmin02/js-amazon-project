0654c0d8849eaabf53d401da0f4934189b30ea27
ShreyansAmin02 <shreyans.amin02@gmail.com>
2025-01-19 13:32:50 +1000

    Refactor cart management by introducing loadFromStorage function; add cart tests and update test paths

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }]; // default cart
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }]; // default cart
+  }
 }
 
 function saveToStorage() {
@@ -19,6 +25,7 @@ function saveToStorage() {
 export function addToCart(productId) {
   let matchingItem = '';
 
+  // uncomment this later
   const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`); // select the quantity from 1-10
   const quantity = Number(quantitySelector.value); // transfer to variable
   quantitySelector.value = 1; // reset to 1
