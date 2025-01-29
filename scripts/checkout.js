fa88b532ce2d3a2d658abcf13058eb9df0a75910
ShreyansAmin02 <shreyans.amin02@gmail.com>
2025-01-29 11:38:20 +1000

    Add loadCart function and integrate it into checkout process

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,11 +6,62 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 import { loadProducts } from "../data/products.js";
 
+import { loadCart } from "../data/cart.js";
+
 // import '../data/backend-practice.js';
 
 import '../data/cart-class.js';
-loadProducts(() => {
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1'); // first load products, then 
+    });
+
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value2');
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1'); // first load products, then 
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+  // load the page elements 
+
+}).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+
+});
+*/
